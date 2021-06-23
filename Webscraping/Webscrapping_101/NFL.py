03a22cc4df287225525a78ebf4b69c7135501752
Caio-G <caiogonzaga332@gmail.com>
2021-06-23 15:18:02 -0300

    NFL

--- Webscraping/Webscrapping_101/NFL.py
+++ Webscraping/Webscrapping_101/NFL.py
@@ -4,7 +4,7 @@ import json
 import pandas as pd
 from selenium import webdriver
 
-# pegar o conteúdo
+# Pegar o conteúdo
 rankingtop10 ={}
 
 categorias={
@@ -15,27 +15,37 @@ categorias={
     'Interceptações':{'cat':'interceptions','stt':'defensiveinterceptions','label':'INT'},
 }
 
-driver = webdriver.Edge()
-
 def buildrank(type):
     cat = categorias[type]['cat']
     stt = categorias[type]['stt']
     label = categorias[type]['label']
 
-    url = f"https://www.nfl.com/stats/player-stats/category/'{cat}'/2020/REG/all/'{stt}'/DESC"
+    url = f"https://www.nfl.com/stats/player-stats/category/{cat}/2020/REG/all/{stt}/DESC"
     driver.get(url)
 
     element = driver.find_element_by_xpath("//*[@id='main-content']/section[3]/div/div/div/div/table")
     html_content = element.get_attribute('outerHTML')
-
+    
+    # Parsear    
     soup = BeautifulSoup(html_content,'html.parser')
     table = soup.find(name='table')
 
+    # Estruturar
     df_full = pd.read_html(str(table))[0].head(10)
     df = df_full[['Player',label]]
+    # Transformar os dados em um dicionário
+    return df.to_dict('records')
 
-    return
-# Parsear
-# Estruturar
-# Transformar
-# Salvar
+
+driver = webdriver.Edge()
+for k in categorias:
+        rankingtop10[k]=buildrank(k)
+
+driver.quit()
+
+# Converter e Salvar em JSON
+
+js = json.dumps(rankingtop10)
+fp = open('rankingNFL.json','w')
+fp.write(js)
+fp.close()
